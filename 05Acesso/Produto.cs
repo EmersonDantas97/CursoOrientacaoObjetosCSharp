a83462758890ecf6cf9b34c221064a065664107b
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2022-10-03 22:23:58 -0300

    Aula007

---
+++ 05Acesso/Produto.cs
@@ -0,0 +1,13 @@
+class Produto
+{
+    /*
+    Modificadores de acesso
+    public      = Atributos e metodos visiveis em qualquer classes.
+    private     = Atributos e metodos visiveis apenas na classe onde são criados.
+    protected   = Atributos e metodos visiveis em classes onde são criados ou herdados.
+    */
+
+    public string nome;
+    private double valor;
+
+}
