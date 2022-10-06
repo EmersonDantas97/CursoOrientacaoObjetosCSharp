bb16ba3741ded08512918d71c0bca053d14f2257
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2022-10-05 22:52:34 -0300

    Aula012

---
+++ 10Heranca/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _10Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colaborador obj = new Colaborador("Emerson",25,2000);
+
+        }
+    }
+}
